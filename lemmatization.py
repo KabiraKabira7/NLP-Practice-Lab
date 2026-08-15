761b51b1d0592b1df546979a7fa9adc92a64e28d
Tim Kabira <timothykabira@gmail.com>
2026-08-15 16:01:10 +0300

    lemmatization

--- lemmatization.py
+++ lemmatization.py
@@ -12,3 +12,34 @@ print(lemmatizer.lemmatize(word))
 word = "studies"
 
 print(lemmatizer.lemmatize(word))
+
+
+import nltk
+
+nltk.download("wordnet")
+
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+words = [
+    "playing",
+    "played",
+    "studies",
+    "studying",
+    "running",
+    "better"
+]
+
+for word in words:
+    print(word, "→", lemmatizer.lemmatize(word))
+    
+
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+print(lemmatizer.lemmatize("playing", pos="v"))
+print(lemmatizer.lemmatize("played", pos="v"))
+print(lemmatizer.lemmatize("running", pos="v"))    
+    
