0091ca20b9c49dbc6bc386559baddd9d11797b74
Tim Kabira <timothykabira@gmail.com>
2026-08-16 09:18:10 +0300

    pos_tagging.py

--- pos_tagging.py
+++ pos_tagging.py
@@ -1,4 +1,5 @@
 import nltk
+from nltk.stem import WordNetLemmatizer
 
 nltk.download("averaged_perceptron_tagger_eng")
 
@@ -11,4 +12,32 @@ tokens = word_tokenize(text)
 
 tags = pos_tag(tokens)
 
+print(tags)
+
+
+sentences = [
+    "I will book a flight.",
+    "I read a book."
+]
+
+for sentence in sentences:
+    tokens = word_tokenize(sentence)
+    tags = pos_tag(tokens)
+
+    print("\nSentence:")
+    print(sentence)
+
+    print("POS tags:")
+    print(tags)
+    
+    
+    lemmatizer = WordNetLemmatizer()
+
+text = "The students are studying and playing football."
+
+tokens = word_tokenize(text)
+
+tags = pos_tag(tokens)
+
+print("POS tags:")
 print(tags)
